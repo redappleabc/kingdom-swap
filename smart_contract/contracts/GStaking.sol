4634b55d16a3f67a9f1aff8dfd92a8293b527ea2
redapple961129 <redapple961129@hotmail.com>
2022-05-02 23:47:55 +0900

    gstaking renamed

--- smart_contract/contracts/GStaking.sol
+++ smart_contract/contracts/GStaking.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract LPFarming is Ownable {
+contract GovernanceStaking is Ownable {
     //contract name
     string public name = "Governance Token Staking";
 
