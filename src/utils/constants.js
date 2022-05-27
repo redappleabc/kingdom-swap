b3e8fcd6639c9918b1c27df24ec03f1680785e8e
redapple961129 <redapple961129@hotmail.com>
2022-05-28 02:50:24 +0900

    bsc token

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,12 +8,12 @@ export const gStakingContractABI = GStakingABI;
 
 export const stakingContractAddress = '0x9B7B7F80D461488F78756E195AE6042ebeE2BC2f';
 
-export const gStakingContractAddress = "0xFfE0aC9d4b5b81694526028d9bdc106B9186d8F0"//'0xd24969130251ba4562e183199a6e75063c1c94db';
+export const gStakingContractAddress = "0x59A995f8b8ea0c2E72aCe771c19eE4F43dB250CC"//"0xFfE0aC9d4b5b81694526028d9bdc106B9186d8F0"//'0xd24969130251ba4562e183199a6e75063c1c94db';
 
 export const tokenContractABI = tokenABI;
-export const tokenContractAddress = '0xD6c7EdA05CB5e39A81F04944467871c8228F5f22';
+export const tokenContractAddress = '0x4e7181434f6fc45ca4a7fc88a5c0201cd781ace7';
 
 
-export const reflectionTokenContractAddress = '0xaDD643208f8A4bC882c18288C05651F471519eAC';
+export const reflectionTokenContractAddress = '0xb6b3f3A06D7cCF785a1E1b2aD4b018e3E57Fa6Ee';
 
 
